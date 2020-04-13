df84dabfce4c741cb4a9810413d555a4bd719bb2
Imad Toubal <imad.toubal@gmail.com>
2020-04-13 00:05:33 -0500

    refactor

--- facetrack.py
+++ facetrack.py
@@ -1,15 +1,15 @@
 import cv2
-import numpy as np 
+import numpy as np
 import math
+import argparse
+import os
 
-path_to_video = 'video.mp4'
-margin = 80
-threshold = 10
 
 def lerp(a, b, p):
     return math.floor(a + (b - a) * p)
 
-def image_resize(image, width = None, height = None, inter = cv2.INTER_AREA):
+
+def image_resize(image, width=None, height=None, inter=cv2.INTER_AREA):
     # initialize the dimensions of the image to be resized and
     # grab the image size
     dim = None
@@ -35,96 +35,116 @@ def image_resize(image, width = None, height = None, inter = cv2.INTER_AREA):
         dim = (width, int(h * r))
 
     # resize the image
-    resized = cv2.resize(image, dim, interpolation = inter)
+    resized = cv2.resize(image, dim, interpolation=inter)
 
     # return the resized image
     return resized
 
 
-cap = cv2.VideoCapture(path_to_video)
-face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--inpath', help="Path to the input videos directory.")
+    parser.add_argument('--size', help="Output size.")
+    args = parser.parse_args()
 
-first_frame = True
-f = 0
-x = -1
-while(True):
-    # Capture frame-by-frame
-    ret, frame = cap.read()
+    out_size = int(args.size)
+    threshold = 10
+    margin = 80
 
-    # Our operations on the frame come here
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    video_names = [vpath for vpath in os.listdir(
+        args.inpath) if vpath[-4:] == '.mp4']
 
-    gray_re = image_resize(gray, width=400)
-    
-    height, width = gray.shape
-    height_re, width_re = gray_re.shape
+    for video_name in video_names:
+        path_to_video = os.path.join(args.inpath, video_name)
+        output_path = os.path.join(
+            args.inpath, f'{video_name[:-4]}_{str(out_size)}.avi')
 
-    ratio = height / height_re
+        out = cv2.VideoWriter(output_path, cv2.VideoWriter_fourcc(
+            'M', 'J', 'P', 'G'), 30, (out_size, out_size))
 
-    # Detect the faces
-    faces = face_cascade.detectMultiScale(gray_re, 1.1, 5)
+        cap = cv2.VideoCapture(path_to_video)
+        face_cascade = cv2.CascadeClassifier(
+            'haarcascade_frontalface_default.xml')
 
-    # Draw the rectangle around each face
-    maxArea = 0
-    minDist = np.inf
-    
-    
-    for (_x, _y, _w, _h) in faces:
-        # cv2.rectangle(gray, (_x, _y), (x+fw, y+fh), (255, 0, 0), 2)
-        if  first_frame and _w*_h > maxArea:
-            x, y, w, h = _x, _y, _w, _h
-    
-        elif (_x - _xp) ** 2 + (_y - _yp) ** 2 < minDist:
-            x, y, w, h = _x, _y, _w, _h
-            minDist = (_x - _xp) ** 2 + (_y - _yp) ** 2  
-            
-        maxArea = w*h
-        _xp = x
-        _yp = y
+        first_frame = True
+        f = 0
+        x = -1
 
+        while(True):
+            # Capture frame-by-frame
+            ret, frame = cap.read()
+            if not ret:
+                break
+            # Our operations on the frame come here
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    #If one or more faces are found, draw a rectangle around the
-    #largest face present in the picture
-    if maxArea > 0 :
-        if minDist < threshold:
-            # add margin
-            x = max(math.floor(x * ratio - margin), 0)
-            y = max(math.floor(y * ratio - margin), 0)
-        if first_frame:
-            fw = math.floor(w * ratio + 2 * margin)
-            fh = math.floor(h * ratio + 2 * margin) 
-        
-    # get rectangle
-    if not first_frame:
-        lerp_p = .3
-        if minDist < threshold:
-            x, y = lerp(xp, x, lerp_p), lerp(yp, y, lerp_p)
-        else:
-            x, y = xp, yp
-        
+            gray_re = image_resize(gray, width=400)
 
-    if x >= 0:
-        patch = gray[y:y+fh, x:x+fw]
-        # Convert patch to feature vector
+            height, width = gray.shape
+            height_re, width_re = gray_re.shape
 
-        xp, yp = x, y
-    
-    # Display the resulting frame
-    try:
-        cv2.imwrite('output_{}.png'.format(f), patch)
-        f = f + 1
-        # cv2.imshow('frame', gray)
-    except:
-        print('output/finding face')
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+            ratio = height / height_re
 
-    # check if faces were found
-    if (x >= 0):
-        first_frame = False
-    
+            # Detect the faces
+            faces = face_cascade.detectMultiScale(gray_re, 1.1, 5)
 
+            # Draw the rectangle around each face
+            maxArea = 0
+            minDist = np.inf
 
-# When everything done, release the capture
-cap.release()
-cv2.destroyAllWindows()
+            for (_x, _y, _w, _h) in faces:
+                # cv2.rectangle(gray, (_x, _y), (x+fw, y+fh), (255, 0, 0), 2)
+                if first_frame and _w*_h > maxArea:
+                    x, y, w, h = _x, _y, _w, _h
+
+                elif (_x - _xp) ** 2 + (_y - _yp) ** 2 < minDist:
+                    x, y, w, h = _x, _y, _w, _h
+                    minDist = (_x - _xp) ** 2 + (_y - _yp) ** 2
+
+                maxArea = w*h
+                _xp = x
+                _yp = y
+            # If one or more faces are found, draw a rectangle around the
+            # largest face present in the picture
+            if maxArea > 0:
+                if minDist < threshold:
+                    # add margin
+                    x = max(math.floor(x * ratio - margin), 0)
+                    y = max(math.floor(y * ratio - margin), 0)
+                if first_frame:
+                    fw = math.floor(w * ratio + 2 * margin)
+                    fh = math.floor(h * ratio + 2 * margin)
+
+            # get rectangle
+            if not first_frame:
+                lerp_p = .3
+                if minDist < threshold:
+                    x, y = lerp(xp, x, lerp_p), lerp(yp, y, lerp_p)
+                else:
+                    x, y = xp, yp
+
+            if x >= 0:
+                patch = frame[y:y+fh, x:x+fw]
+                # Convert patch to feature vector
+
+                xp, yp = x, y
+
+            # Display the resulting frame
+            try:
+                # cv2.imwrite('output_{}.png'.format(f), patch)
+                f = f + 1
+                frameout = image_resize(patch, width=out_size, height=out_size)
+                out.write(frameout)
+
+            except:
+                print('output/finding face')
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+
+            # check if faces were found
+            if (x >= 0):
+                first_frame = False
+
+        cap.release()
+        out.release()
+        cv2.destroyAllWindows()
--- readme.md
+++ readme.md
@@ -1,38 +1,39 @@
 # Face tracking with OpenCV
+
 ![](assets/preview.gif)
 
 ## Getting Started (using Python virtualenv)
 
 You need to have Python installed in your computer.
 
-1. Install `virtualenv`: 
-    ```
-    pip install virtualenv
-    ```
+1. Install `virtualenv`:
+   ```
+   pip install virtualenv
+   ```
 2. Create a Python virtual environment:
-    ```
-    virtualenv venv
-    ```
+   ```
+   virtualenv venv
+   ```
 3. Activate virtual environment:
-    1. Windows:
-    ```
-    cd venv\Scripts
-    activate
-    cd ..\..
-    ```
-    2. Lunix / Mac:
-    ```
-    source venv/bin/activate
-    ```
+   1. Windows:
+   ```
+   cd venv\Scripts
+   activate
+   cd ..\..
+   ```
+   2. Lunix / Mac:
+   ```
+   source venv/bin/activate
+   ```
 4. Install libraries:
-   
+
    ```
    pip install -r requirements.txt
    ```
 
 ## Run the code
 
-* Run the app:
-    ```
-    python facetrack.py
-    ```
+- Run the app:
+  ```
+  python facetrack.py --inpath <input videos directory> --size <desired width and height (as a single number)>
+  ```
